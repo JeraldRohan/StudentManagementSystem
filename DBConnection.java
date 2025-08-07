4c169bc29f9df70b4f259e50f7db7c86a5a0bc1e
JeraldRohan <132476397+JeraldRohan@users.noreply.github.com>
2025-08-07 20:47:23 +0530

    Update DBConnection.java

--- DBConnection.java
+++ DBConnection.java
@@ -4,10 +4,11 @@ import java.sql.SQLException;
 
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/studentdb";
-    private static final String USER = "root";
-    private static final String PASSWORD = "Rohanraj001$";
+    private static final String USER = "YourUsername";
+    private static final String PASSWORD = "YourPassword";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
+
